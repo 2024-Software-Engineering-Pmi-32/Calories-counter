74a7cb96d1e18e30112104f866f0ec69776581f8
Anastasia Dubrovska <114603519+anastasiia-dubrovska@users.noreply.github.com>
2024-11-25 15:47:05 +0200

    Delete ApplicationCode directory

--- ApplicationCode/ClassLibrary.BLL/FoodEntry.cs
+++
@@ -1,9 +0,0 @@
-namespace WpfAppCaloriesCounter.BLL
-{
-    public class FoodEntry
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public int Calories { get; set; }
-    }
-}
